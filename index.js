bb9799d79e5f8f5e7aa086a56b09cd3e264d2c54
Lucas Bonato <lucas.perez.bonato@gmail.com>
2023-04-22 20:41:35 -0300

    Base

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let url = "https://pokeapi.co/api/v2/pokemon/2/";
+
+
+
+function loadPk() {
+    console.log("Dentro");
+    fetch(url) 
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            
+            let name = document.getElementById('name');
+            let img = document.getElementById('picture');
+            let id = document.getElementById('pokeNum');
+            
+            name.innerHTML = data['name'].toUpperCase();
+            let imgData = data['sprites']['front_default']
+            img.setAttribute('src', imgData);
+            id.innerHTML = data['id'];
+        })
+        .catch((error) => {
+            console.log(`Erro: ${error}`);
+        })
+}
